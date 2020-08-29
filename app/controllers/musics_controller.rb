e63795e2b22f2a4fa4b20b944e199b3fe871bf2c
jnuzzi4 <jnuzzi47@gmail.com>
2020-08-29 13:21:01 -0500

    seeded

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -3,32 +3,32 @@ class MusicsController < ApplicationController
     before_action only: [:show, :update]
     
     def index
-        # if params[:limit].present?
-        #     limit = params[:limit]
-        # else
-        #     limit = 10
-        # end
-        # if params[:offset].present?
-        #     offset = params[:offset]
-        # else
-        #     offset = 0
-        # end
+        if params[:limit].present?
+            limit = params[:limit]
+        else
+            limit = 10
+        end
+        if params[:offset].present?
+            offset = params[:offset]
+        else
+            offset = 0
+        end
 
-        # if params[:user_id].present?
-        #     musics = User.find(params[:user_id]).musics.limit(limit).offset(offset)
-        # elsif params[:search].present?
-        #     musics = Music.where(nil)
-        #     musics = musics.filter_by_search_term(params[:search]).limit(limit).offset(offset)
-        # else
-        #     musics = Music.limit(limit).offset(offset)
-        # end
-        require 'csv'
-        musics = CSV.read("709783.csv",  "r:ISO-8859-1")
+        if params[:user_id].present?
+            musics = User.find(params[:user_id]).musics.limit(limit).offset(offset)
+        elsif params[:search].present?
+            musics = Music.where(nil)
+            musics = musics.filter_by_search_term(params[:search]).limit(limit).offset(offset)
+        else
+            musics = Music.limit(limit).offset(offset)
+        end
+        # require 'csv'
+        # musics = CSV.read("Sheet Music Plus Music Links - Sheet1.csv")
         render json: musics, except: [:created_at, :updated_at]
     end
 
     def show
-        render json: @musics, except: [:created_at, :updated_at]
+        render json: @music, except: [:created_at, :updated_at]
     end
 
     def update
